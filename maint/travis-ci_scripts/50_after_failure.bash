4a09eadb5fe87245f50883894e3e9a1577777bf8
Peter Rabbitson <ribasushi@leporine.io>
2018-01-31 09:56:28 +0100

    die die die

--- maint/travis-ci_scripts/50_after_failure.bash
+++ maint/travis-ci_scripts/50_after_failure.bash
@@ -3,7 +3,7 @@
 # this file is executed in a subshell - set up the common stuff
 source maint/travis-ci_scripts/common.bash
 
-echo_err "$(dmesg)"
+echo "$(dmesg)"
 
 if [[ -n "$SHORT_CIRCUIT_SMOKE" ]] ; then exit 0 ; fi
 
